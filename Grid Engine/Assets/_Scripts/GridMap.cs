bc9e1fac90790418186739053e3212170167f1a5
LivingValkyrie <deadwynn@gmail.com>
2016-01-05 11:24:48 -0600

    added grid generation and base tile class
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

---
+++ Grid Engine/Assets/_Scripts/GridMap.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.com
+/// 
+/// Description: GridMap 
+/// </summary>
+public class GridMap : MonoBehaviour {
+    #region Fields
+
+    public Vector2 mapSize;
+    public GameObject tilePrefab;
+
+    #endregion
+
+    public void GenerateMap() {
+        for (int x = 0; x < mapSize.x; x++) {
+            for (int y = 0; y < mapSize.y; y++) {
+                Vector3 tilePos = new Vector3(-mapSize.x / 2 + 0.5f + x, -mapSize.y / 2 + 0.5f + y);
+                GameObject currTile = Instantiate(tilePrefab, tilePos, Quaternion.identity) as GameObject;
+                currTile.transform.SetParent(this.transform);
+                currTile.name = "Tile " + ((x * mapSize.y) + y);
+                currTile.AddComponent<GridTile>();
+            }
+        }
+    }
+
+}
---
+++ Grid Engine/Assets/_Scripts/GridTile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.com
+/// 
+/// Description: GridTile
+/// </summary>
+public class GridTile : MonoBehaviour {
+    #region Fields
+
+    public TileClass tileClass;
+
+    #endregion
+}
+
+public enum TileType {
+    Walkable,
+    Unwalkable
+}
+
+public enum TileClass {
+    Dirt,
+    Water,
+    Stone
+}
